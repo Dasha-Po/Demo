bfbddf13c87af7a9c1b314fe02bb490e908a7750
Darya Potapova <muljykina.darya@gmail.com>
2022-09-27 23:31:53 +0300

    Google Analytics

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Преподаватель создает еще файл");
         System.out.println("PayButton");
+        System.out.println("Google Analytics");
     }
 }
